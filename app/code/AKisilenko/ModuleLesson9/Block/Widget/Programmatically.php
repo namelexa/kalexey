f7ec9c99dbbac5329b2414e2542426bc543ad1cc
namelexa <namelexa@gmail.com>
2019-01-09 15:57:55 +0200

    Fixed issues

--- app/code/AKisilenko/ModuleLesson9/Block/Widget/Programmatically.php
+++ app/code/AKisilenko/ModuleLesson9/Block/Widget/Programmatically.php
@@ -4,6 +4,7 @@ namespace AKisilenko\ModuleLesson9\Block\Widget;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
+use Magento\Cms\Block\Block;
 
 class Programmatically extends Template implements BlockInterface
 {
@@ -14,4 +15,13 @@ class Programmatically extends Template implements BlockInterface
 
     protected $_template = 'widget/homepagebanner.phtml';
 
+    public function getHomeBanner()
+    {
+        return $this
+            ->getLayout()
+            ->createBlock('Magento\Cms\Block\Block')
+            ->setBlockId($this->getData('select_block'))
+            ->toHtml();
+    }
+    
 }
