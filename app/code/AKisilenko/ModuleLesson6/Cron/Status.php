310ed22aacff35b180ab6fd213c45e03c8d36056
namelexa <namelexa@gmail.com>
2019-02-28 22:58:26 +0200

    Add group general, Cron, Test and dynamical-row

--- app/code/AKisilenko/ModuleLesson6/Cron/Status.php
+++ app/code/AKisilenko/ModuleLesson6/Cron/Status.php
@@ -3,7 +3,10 @@ namespace AKisilenko\ModuleLesson6\Cron;
 
 use AKisilenko\ModuleLesson6\Model\AskQuestion;
 use AKisilenko\ModuleLesson6\Model\ResourceModel\AskQuestion\CollectionFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Psr\Log\LoggerInterface;
+use Magento\Store\Model\ScopeInterface;
+use AKisilenko\ModuleLesson6\Helper\Config;
 
 /**
  * Class Status
@@ -11,8 +14,13 @@ use Psr\Log\LoggerInterface;
  */
 class Status
 {
-    const DAYS_QUANTITY = 3;
-
+    /**
+     * @var
+     */
+    protected $scopeConfig;
+    /**
+     * @var CollectionFactory
+     */
     protected $askFactory;
     /**
      * @var LoggerInterface
@@ -22,38 +30,67 @@ class Status
     /**
      * Status constructor.
      * @param LoggerInterface $logger
+     * @param ScopeConfigInterface $scopeConfig
      * @param CollectionFactory $askQuestionsFactory
      */
     public function __construct(
         LoggerInterface $logger,
+        ScopeConfigInterface $scopeConfig,
+//        Config $config,
         CollectionFactory $askQuestionsFactory
     ) {
+//        $this->dateQuantity = $config;
         $this->logger = $logger;
+        $this->scopeConfig = $scopeConfig;
         $this->askFactory = $askQuestionsFactory;
     }
 
     /**
+     * @param string $key
+     * @param string $section
      * @return mixed
      */
-    protected function getDaysQuantity()
+    private function isEnabled($key = 'enable_cron', $section = 'cron')
     {
-        return self::DAYS_QUANTITY;
+        return $this->scopeConfig
+            ->getValue(
+                "ask_question_options/$section/$key",
+                ScopeInterface::SCOPE_STORE
+            );
+    }
+
+    /**
+     * @param string $key
+     * @param string $section
+     * @return mixed
+     */
+    private function getDaysQuantity($key = 'frequency', $section = 'cron')
+    {
+        return $this->scopeConfig
+            ->getValue(
+                "ask_question_options/$section/$key",
+                ScopeInterface::SCOPE_STORES
+            );
     }
 
     public function execute()
     {
-        $currentDate = date('Y-m-d h:i:s');
-        $filterDateTime = strtotime('-' . $this->getDaysQuantity() . ' day', strtotime($currentDate));
-        $filterDate = date('Y-m-d h:i:s', $filterDateTime);
+        if ($this->isEnabled() == 1) {
+            $currentDate = date('Y-m-d h:i:s');
+            $filterDateTime = strtotime('-' . $this->getDaysQuantity() . ' day', strtotime($currentDate));
+            $filterDate = date('Y-m-d h:i:s', $filterDateTime);
 
-        $askQuestions = $this->askFactory->create();
-        $collection = $askQuestions
-            ->addFieldToFilter('status', ['eq' => AskQuestion::STATUS_PENDING])
-            ->addFieldToFilter('created_at', ['lt' => $filterDate]);
+            $askQuestions = $this->askFactory->create();
+            $collection = $askQuestions
+                ->addFieldToFilter('status', ['eq' => AskQuestion::STATUS_PENDING])
+                ->addFieldToFilter('created_at', ['lt' => $filterDate]);
 
-        foreach ($collection as $item) {
-            $item->setStatus(AskQuestion::STATUS_PROCESSED)->save();
+            foreach ($collection as $item) {
+                $item->setStatus(AskQuestion::STATUS_PROCESSED)->save();
+            }
+            $this->logger->critical('Change status completed');
+        } else {
+            $this->logger->critical('Change status is off');
         }
-        $this->logger->critical('Test status');
     }
 }
