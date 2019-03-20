deda9a0bac1e65fa8524b5601681fc46cb39ae5a
namelexa <namelexa@gmail.com>
2019-03-20 21:45:14 +0200

    Send email

---
+++ app/code/AKisilenko/ModuleLesson6/Helper/Mail.php
@@ -0,0 +1,105 @@
+<?php
+namespace AKisilenko\ModuleLesson6\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Framework\Translate\Inline\StateInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\User\Model\UserFactory;
+
+class Mail extends AbstractHelper
+{
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+    /**
+     * @var TransportBuilder
+     */
+    private $transportBuilder;
+    /**
+     * @var StateInterface
+     */
+    private $inlineTranslation;
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
+    private $userFactory;
+    /**
+     * Mail constructor.
+     * @param Context $context
+     * @param StoreManagerInterface $storeManager
+     * @param TransportBuilder $transportBuilder
+     * @param StateInterface $inlineTranslation
+     * @param ScopeConfigInterface $scopeConfig
+     * @param UserFactory $userFactory
+     */
+    public function __construct(
+        Context $context,
+        StoreManagerInterface $storeManager,
+        TransportBuilder $transportBuilder,
+        StateInterface $inlineTranslation,
+        ScopeConfigInterface $scopeConfig,
+        UserFactory $userFactory
+    ) {
+        $this->storeManager = $storeManager;
+        $this->transportBuilder = $transportBuilder;
+        $this->inlineTranslation = $inlineTranslation;
+        $this->scopeConfig = $scopeConfig;
+        $this->userFactory = $userFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @param $emailFrom
+     * @param string $customerName
+     * @param $message
+     * @throws \Magento\Framework\Exception\MailException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function sendMail($emailFrom, $customerName = '', $message)
+    {
+        $templateOptions = [
+            'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+            'store' => $this->storeManager->getStore()->getId()
+        ];
+        $templateVars = [
+            'store' => $this->storeManager->getStore(),
+            'name' => $customerName,
+            'comment'   => $message
+        ];
+        $from = ['email' => $emailFrom, 'name' => $customerName];
+        $this->inlineTranslation->suspend();
+        $to = [$this->getAdminEmail()];
+        $transport = $this->transportBuilder->setTemplateIdentifier('ask_question_email')
+            ->setTemplateOptions($templateOptions)
+            ->setTemplateVars($templateVars)
+            ->setFrom($from)
+            ->addTo($to)
+            ->getTransport();
+        $transport->sendMessage();
+        $this->inlineTranslation->resume();
+    }
+    /**
+     * @return mixed|string
+     */
+    private function getAdminEmail()
+    {
+        $transEmailSaller = $this->scopeConfig->getValue(
+            'trans_email/ident_sales/email',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        if ($transEmailSaller) {
+            return $transEmailSaller;
+        }
+        $userFactory =  $this->userFactory->create();
+        if ($userFactory) {
+            $user = $userFactory->getById(1);
+            return $user->getEmail();
+        }
+        return '';
+    }
+}
